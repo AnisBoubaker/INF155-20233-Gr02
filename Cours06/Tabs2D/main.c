386769d37a97601585533607d1675862bc989bdc
Anis Boubaker <anis@boubaker.ca>
2023-10-19 12:17:07 -0400

    Cours 06

--- Cours06/Tabs2D/main.c
+++ Cours06/Tabs2D/main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define MAX_LIGNES 10
-#define MAX_COLONNES 20
-
-double moyenne_tab2d(double tab[][MAX_COLONNES], int nb_lignes, int nb_colonnes);
-void afficher_tab2d(double tab[][MAX_COLONNES], int nb_lignes, int nb_colonnes);
-
+/*
+ * NE JAMAIS INCLURE LE .C
+ */
+#include "temperatures.h"
+#include "test/module_test.h"
 
 int main(void) {
     int nb_lignes, nb_colonnes;
@@ -47,65 +45,6 @@ int main(void) {
 
     return EXIT_SUCCESS;
 }
-
-/*
- * Lorsqu'on passe un tableau à 2 dimensions à une fonction, il
- * est obligatoire de spécifier la taille MAXIMALE en colonnes.
- */
-void afficher_tab2d(double tab[][MAX_COLONNES], int nb_lignes, int nb_colonnes)
-{
-    for(int i=0; i < nb_lignes ; i++)
-    {
-        for(int j=0; j< nb_colonnes; j++)
-        {
-            printf("%.2lf\t", tab[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-/* Écrire une fonction `moyenne_tab2d` qui reçoit
- * un tableau à 2 dimensions de réels et qui retourne
- * la moyenne du tableau. */
-double moyenne_tab2d(double tab[][MAX_COLONNES], int nb_lignes, int nb_colonnes)
-{
-    double somme = 0 ;
-
-    for(int i=0; i< nb_lignes; i++)
-    {
-        for(int j =0 ; j<nb_colonnes ; j++)
-        {
-            somme += tab[i][j];
-        }
-    }
-    return somme / (nb_lignes * nb_colonnes);
-}
-
-/*
- * Écrire la fonction `temperatures_excedant` compter le nombre de temperatures
- * qui excèdent une température donnée
- */
-
-int temperature_excedant(double tab[][MAX_COLONNES],
-                         int nb_lignes,
-                         int nb_colonnes,
-                         double temp_seuil)
-{
-    int nb_excedant = 0;
-
-    for(int i=0; i<nb_lignes; i++)
-    {
-        for(int j=0; j<nb_colonnes; j++)
-        {
-            if(tab[i][j]>temp_seuil)
-            {
-                nb_excedant++;
-            }
-        }
-    }
-    return nb_excedant;
-}
-
 
 
 
