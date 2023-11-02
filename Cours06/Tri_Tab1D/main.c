703b5e6f5ad045a07228aa440c33448371e439d1
Anis Boubaker <anis@boubaker.ca>
2023-11-02 09:58:37 -0400

    Cours 07

--- Cours06/Tri_Tab1D/main.c
+++ Cours06/Tri_Tab1D/main.c
@@ -1,33 +1,16 @@
 #include <stdio.h>
-
-void tri_insertion(int tab[], int taille);
+#include "tri_tab1d.h"
 
 int main() {
     int mon_tableau[] = {45, 10, 2, 28, 55, 46, 78, 32};
+    int mon_tableau2[] = {45, 10, 2, 28, 55, 46, 78, 32};
 
     tri_insertion(mon_tableau, 8);
+    afficher_tab(mon_tableau, 8);
 
-    for(int i=0; i<8; i++)
-    {
-        printf("%i ", mon_tableau[i]);
-    }
+    tri_selection(mon_tableau2, 8);
+    afficher_tab(mon_tableau2, 8);
 
     return 0;
 }
 
-void tri_insertion(int tab[], int taille)
-{
-    int x;
-    int j;
-    for(int i=1; i<taille; i++)
-    {
-        x = tab[i];
-        j = i;
-        while(j>0 && tab[j-1] > x)
-        {
-            tab[j] = tab[j-1];
-            j--;
-        }
-        tab[j] = x;
-    }
-}
