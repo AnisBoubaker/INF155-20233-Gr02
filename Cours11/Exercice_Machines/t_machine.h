6c00cb12882648e7fb3e2e7db74ad18078744d01
Anis Boubaker <anis@boubaker.ca>
2023-11-30 10:00:29 -0500

    Cours 11

--- Cours11/Exercice_Machines/t_machine.h
+++ Cours11/Exercice_Machines/t_machine.h
@@ -52,5 +52,16 @@ void machine_afficher(const t_machine* m);
 void machine_jeu_machines(t_machine* machines[], int taille_max);
 
 
+/*
+Identifie toutes les machines qui doivent subir une opération de maintenance.
+Ces machines sont celles dont la date de dernière maintenance est nulle (0-0-0)
+ou celles dont la date de dernière maintenance est antérieure à une date_min. ENTREES:
+- liste_machines: Tableau de références des machines du parc
+- nb_machines: nombre de machines dans le tableau liste_machines
+- date_min: Date minimale
+SORTIE:
+Tableau dynamique de références vers des machines qui doivent être maintenues.
+*/
+t_machine** machines_a_maintenir(t_machine* liste_machines[], int nb_machines, t_date date_min,int* nb_a_maintenir);
 
 #endif //EXERCICE_MACHINES_T_MACHINE_H
